51a7775db280a1f13e658942f1d634abf1875ce5
czanikan <32414135+czanikan@users.noreply.github.com>
2020-11-11 20:48:23 +0100

    New Features
    Made by Tünde Tutor

---
+++ Assets/Scripts/BoxData.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoxData : MonoBehaviour
+{
+    public string stamps = "";
+
+    void Start()
+    {
+        stamps = "";
+    }
+    public void AddStamp(char stamp)
+    {
+        stamps += stamp;
+    }
+}
---
+++ Assets/Scripts/StampMachine.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StampMachine : MonoBehaviour
+{
+    public float speed = 2;
+    public Transform endpoint;
+    public enum colorType {Red, Blue};
+    public colorType ct;
+    Renderer renderer;
+
+    void Start()
+    {
+        renderer = GetComponent<Renderer>();
+        ct = colorType.Red;
+    }
+
+    void Update()
+    {
+        if(GetComponent<DragObject>().dragging)
+        {
+            if(Input.GetKeyDown(KeyCode.Return))
+            {
+                if(ct == colorType.Red)
+                {
+                    ct = colorType.Blue;
+                    renderer.materials[2].SetColor("_Color", Color.blue);
+                }
+                else
+                {
+                    ct = colorType.Red;
+                    renderer.materials[2].SetColor("_Color", Color.red);
+                }
+            }
+        }
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        
+        other.transform.position = Vector3.MoveTowards(other.transform.position, endpoint.position, speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (ct == colorType.Red)
+        {
+            other.GetComponent<BoxData>().AddStamp('r');
+        }
+        else
+        {
+            other.GetComponent<BoxData>().AddStamp('b');
+        }
+    }
+}
