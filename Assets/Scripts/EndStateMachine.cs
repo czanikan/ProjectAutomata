2dce499ab837ecde58c3e1505cc7acafcaef2d9b
czanikan <32414135+czanikan@users.noreply.github.com>
2020-11-12 11:48:19 +0100

    v.0.3 Update
    -add end state machine to the game
    -add task to the game
    -add demo level 1
    -add task checker

---
+++ Assets/Scripts/EndStateMachine.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EndStateMachine : MonoBehaviour
+{
+    public Transform returnpoint;
+    public Transform checkpoint;
+    public float speed = 2;
+    private string boxString;
+    private bool returning = false;
+    
+    void Start()
+    {
+        returning = false;
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Im on my waaay");
+        boxString = other.GetComponent<BoxData>().stamps;
+        
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if(!returning && Vector3.Distance(other.transform.position, checkpoint.transform.position) > 0.01f)
+        {
+            other.transform.position = Vector3.MoveTowards(other.transform.position, checkpoint.position, speed * Time.deltaTime);
+            if (boxString == "rbrbrb")
+            {
+                Debug.Log("Complete! :3");
+            }
+            else
+            {
+                Debug.Log("return to destination");
+                returning = true;
+            }
+        }
+        if(returning)
+        {
+            other.transform.position = Vector3.MoveTowards(other.transform.position, returnpoint.position, speed * Time.deltaTime);
+        }
+    }
+}
